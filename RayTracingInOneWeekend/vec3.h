39ef8d1e09aca01ceeee11a7639ec0ad982fd9b4
marie-bisbo <mariebisbo1908@gmail.com>
2023-02-05 14:37:17 +0000

    Add Vec3 utility functions

--- RayTracingInOneWeekend/vec3.h
+++ RayTracingInOneWeekend/vec3.h
@@ -53,3 +53,58 @@ public:
 
 using Point3 = Vec3;
 using Colour = Vec3;
+
+// Utility functions
+
+inline std::ostream& operator<<(std::ostream& out, const Vec3& vector)
+{
+	return out << vector.vec[0] << vector.vec[1] << vector.vec[2];
+}
+
+inline Vec3 operator+(const Vec3& u, const Vec3& v)
+{
+	return Vec3(u.vec[0] + v.vec[0], u.vec[1] + v.vec[1], u.vec[2] + v.vec[2]);
+}
+
+inline Vec3 operator-(const Vec3& u, const Vec3& v)
+{
+	return Vec3(u.vec[0] - v.vec[0], u.vec[1] - v.vec[1], u.vec[2] - v.vec[2]);
+}
+
+inline Vec3 operator*(const Vec3& u, const Vec3& v)
+{
+	return Vec3(u.vec[0] * v.vec[0], u.vec[1] * v.vec[1], u.vec[2] * v.vec[2]);
+}
+
+inline Vec3 operator*(double scalar, const Vec3& v)
+{
+	return Vec3(scalar * v.vec[0], scalar * v.vec[1], scalar * v.vec[2]);
+}
+
+inline Vec3 operator*(const Vec3& v, double scalar)
+{
+	return scalar * v;
+}
+
+inline Vec3 operator/(const Vec3& v, double scalar)
+{
+	return (1 / scalar) * v;
+}
+
+inline double Dot(const Vec3& u, const Vec3& v)
+{
+	return u.vec[0] * v.vec[0] + u.vec[1] * v.vec[1] + u.vec[2] * v.vec[2];
+}
+
+inline Vec3 Cross(const Vec3& u, const Vec3& v) 
+{
+	return Vec3(u.vec[1] * v.vec[2] - u.vec[2] * v.vec[1],
+		u.vec[2] * v.vec[0] - u.vec[0] * v.vec[2],
+		u.vec[0] * v.vec[1] - u.vec[1] * v.vec[0]);
+}
+
+inline Vec3 UnitVector(const Vec3 v)
+{
+	return v / v.Length();
+}
+
